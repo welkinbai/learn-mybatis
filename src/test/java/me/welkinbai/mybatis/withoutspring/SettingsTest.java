b9e580f4b933d9af07be5f20e33922efc27c7b6a
welkinbai <xwmbxx@126.com>
2017-01-04 22:58:08 +0800

    设置测试： 2. 关于自动生成主键的设置的测试

--- src/test/java/me/welkinbai/mybatis/withoutspring/SettingsTest.java
+++ src/test/java/me/welkinbai/mybatis/withoutspring/SettingsTest.java
@@ -1,5 +1,6 @@
 package me.welkinbai.mybatis.withoutspring;
 
+import me.welkinbai.mybatis.withoutspring.domain.City;
 import me.welkinbai.mybatis.withoutspring.mapper.CityMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -32,19 +33,49 @@ public class SettingsTest {
         sqlSession = sqlSessionFactory.openSession();
     }
 
+    /*
+    * 测试方法(steps)：
+    * 1.mybatisconfig_test_settings.xml cacheEnabled->false
+    * 2.run this unit test and see sql executed twice
+    * 3.mybatisconfig_test_settings.xml cacheEnabled->true
+    * 4.run this unit test again and see sql executed once*/
     @Test
     public void testCloseCache() throws Exception {
+        SqlSession sqlSession2;
+        sqlSession2 = this.sqlSessionFactory.openSession();
         try {
             CityMapper city = sqlSession.getMapper(CityMapper.class);
             System.out.println(city.selectById(1).getName());
             sqlSession.close();
-            SqlSession sqlSession2 = this.sqlSessionFactory.openSession();
             CityMapper city2 = sqlSession2.getMapper(CityMapper.class);
             System.out.println(city2.selectById(1).getName());
         } finally {
             sqlSession.close();
+            sqlSession2.close();
         }
     }
 
+    /*
+    * 测试方法（steps）：
+    * 1. CityMapper.xml insert useGeneratedKeys="true"->just remove it
+    * 2. change mybatisconfig_test_settings.xml useGeneratedKeys->true
+    * 3. run this unit test and see city.getId() return right number. useGeneratedKeys config take effect.
+    * 4. add useGeneratedKeys="false" to CityMapper.xml insert
+    * 5. run this unit test again and see useGeneratedKeys in CityMapper.xml cover useGeneratedKeys in mybatisconfig_test_settings.xml*/
+    @Test
+    public void testUseGeneratedKeys() throws Exception {
+        try {
+            CityMapper cityMapper = sqlSession.getMapper(CityMapper.class);
+            City city = new City();
+            city.setName("test_use_generated_keys");
+            city.setCountryCode("CHN");
+            city.setPopulation(122);
+            city.setDistrict("wuhan");
+            cityMapper.insert(city);
+            System.out.println(city.getId());
+        } finally {
+            sqlSession.close();
+        }
+    }
 
 }
